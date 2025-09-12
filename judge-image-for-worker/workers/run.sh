18673aec7063373d9629c56f9ceae34a01409cf6
lightning-sagar <lightningsagar0@gmail.com>
2025-09-13 04:39:09 +0530

    added the gitops for docker workers and documentation!!

--- judge-image-for-worker/workers/run.sh
+++ judge-image-for-worker/workers/run.sh
@@ -4,3 +4,4 @@ PORT=3003 WORKER_FIELD=worker_2 nodemon index.js &
 
 
 
+# docker run -e password_redis="e5qUny4OV6WiMnOPWt4bRM9NmEmOYa91" -e host_redis="redis-19417.c270.us-east-1-3.ec2.redns.redis-cloud.com" -p 5000:5000
